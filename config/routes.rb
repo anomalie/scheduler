1bf9b82309458fb2ee81ad0e9bc1c906ee8790d7
anomalie <emiliecorpuz@gmail.com>
2017-06-16 09:56:36 -0700

    create ruby scheduler

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'calendar/show'
+  resource :calendar, only: [:show], controller: :calendar
+  
+  root to: "calendar#show"
   root to: 'visitors#index'
   devise_for :users
   resources :users
 end
+
